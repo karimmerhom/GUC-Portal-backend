58d9cef4720cb98534c6f434fbfcf1564628dd39
mohamedhossam11 <moh.queena@gmail.com>
2020-02-09 17:49:10 +0200

    hot fix

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-  postgresURI: 'postgres://postgres:654@localhost:5432/TBH',
+  postgresURI:
+    'postgres://nodems:nodems@odoo.cwkflpbzjgkp.eu-central-1.rds.amazonaws.com:5432/tbh',
   secretOrKey: 'LIRTEN',
   salt: parseInt(10, 10),
   emailAccessKey: 'U2FsdGVkX19obt+1O1fD8EVO9wRD9eTn6bckJOyG7y4=',
