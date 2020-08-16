2a6dee892bf8fd23c45974ffe07b88aa88336867
Fady Samir <fadysamirh@gmail.com>
2020-08-16 15:09:59 +0200

    viewavailablerooms

---
+++ keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  postgresURI:
+    'postgres://tbh_read_write:tbh_read_write@odoo.cwkflpbzjgkp.eu-central-1.rds.amazonaws.com:5432/tbh-dev',
+  secretOrKey: 'SECRET',
+  salt: '10',
+}
