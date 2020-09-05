d71e48fdc0377f82f6bf9ae8159e60d1c4905008
mohamedhossam11 <moh.queena@gmail.com>
2020-09-05 15:08:27 +0200

    fix

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -10,14 +10,14 @@ module.exports = {
   facebookAuth: {
     client_id: '220638332401032',
     client_secret: 'b0c3014038aa4771ff2d6bc725515df5',
-    redirect_uri_login: 'http://localhost:3000/home/SignIn',
-    redirect_uri_sign_up: 'http://localhost:3000/home/SignUp',
+    redirect_uri_login: 'http://localhost:3000/facebook/SignIn',
+    redirect_uri_sign_up: 'http://localhost:3000/facebook/SignUp',
   },
   googleAuth: {
     client_id:
       '790311029308-uok5sjd316a26f6mjhq6ff6rlg1t8dfi.apps.googleusercontent.com',
     client_secret: 'HnlkzK0HJpJUR1BXW1XC_67U',
-    loginURI: 'http://localhost:3000/home/SignIn',
-    signUpURI: 'http://localhost:3000/home/SignUp',
+    loginURI: 'http://localhost:3000/google/SignIn',
+    signUpURI: 'http://localhost:3000/google/SignUp',
   },
 }
