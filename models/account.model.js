79a4fad5e0d02cce0931a52db49fb275e0c399ac
Youssef Fahmy <Yousseffa7my@gmail.com>
2020-12-19 14:07:12 +0200

    replicating tbh

---
+++ models/account.model.js
@@ -0,0 +1,65 @@
+const Sequelize = require('sequelize')
+
+const sequelize = require('../config/DBConfig')
+
+const { accountStatus, userTypes } = require('../api/constants/TBH.enum')
+
+const { Model } = Sequelize
+
+class Account extends Model {}
+Account.init(
+  {
+    username: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    firstName: {
+      type: Sequelize.STRING,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    phone: {
+      type: Sequelize.STRING,
+    },
+    status: {
+      type: Sequelize.ENUM,
+      values: [
+        accountStatus.VERIFIED,
+        accountStatus.PENDING,
+        accountStatus.SUSPENDED,
+      ],
+    },
+    type: {
+      type: Sequelize.ENUM,
+      values: [userTypes.ADMIN, userTypes.USER],
+    },
+    emailVerified: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    birthdate: {
+      type: Sequelize.STRING,
+    },
+    gender: {
+      type: Sequelize.STRING,
+    },
+    facebookId: {
+      type: Sequelize.STRING,
+    },
+    googleId: {
+      type: Sequelize.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+  }
+)
+
+module.exports = Account
