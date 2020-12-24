ef16ebd3fea08e2547fdead1efa34baf18e64f89
Youssef Fahmy <Yousseffa7my@gmail.com>
2020-12-24 20:23:40 +0200

    merge with fady

--- models/leaves.model.js
+++ models/leaves.model.js
@@ -1,17 +1,30 @@
+const { boolean } = require('joi')
 var mongoose = require('mongoose')
+const { leaveStatus, leaveTypes } = require('../api/constants/GUC.enum')
 
 //Define a schema
 var Schema = mongoose.Schema
 
 const leaves = new Schema({
-  academicId : {type: String},
-  date: {type: String},
-  type: {type: String},
-  reasonForCompensation: {type: String},
+  academicId: { type: String },
+  date: { type: String },
+  type: {
+    type: String,
+    enum: [
+      leaveTypes.MATERNITY,
+      leaveTypes.ACCIDENTAL,
+      leaveTypes.ANNUAL,
+      leaveTypes.SICK,
+    ],
+  },
+  reasonForCompensation: { type: String },
   status: {
-    type: String, 
-    default: ("rejected")},
-  comments: {type: String}
+    type: String,
+    enum: [leaveStatus.ACCEPTED, leaveStatus.PENDING, leaveStatus.REJECTED],
+  },
+  repReqId: [{ type: String }],
+  allRepReq: { type: Boolean },
+  comments: { type: String },
 })
 var leavesModel = mongoose.model('leaves', leaves)
 
