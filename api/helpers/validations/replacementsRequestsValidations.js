7dab15fd238edcba24f4206d73bc769027c7d705
Shadi.Nakhla <Shadinakhla14@gmail.com>
2020-12-25 19:12:31 +0200

    validations no id

--- api/helpers/validations/replacementsRequestsValidations.js
+++ api/helpers/validations/replacementsRequestsValidations.js
@@ -4,7 +4,6 @@ const { leaveStatus, leaveTypes } = require('../../../api/constants/GUC.enum')
 const validateCreateReplacementRequest = (req, res, next) => {
     const schema = Joi.object({
       Account: Joi.object({
-        id: Joi.string().required(),
         academicId: Joi.string().required(),
       }).required(),
       academicIdReciever: Joi.string().required(),
